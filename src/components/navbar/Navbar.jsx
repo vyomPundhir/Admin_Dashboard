cc99dab946e1ee973b5e5a037d1456075781f690
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-15 23:12:11 +0530

    Navbar jsx file updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="logo" />
+        <span>Admin</span>
+      </div>
+      <div className="icons flex">
+        <img src="/serach.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notification.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user flex">
+          <img src="/profile.jpg" alt="profile" />
+          <span>Vyom</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
   )
 }
 
