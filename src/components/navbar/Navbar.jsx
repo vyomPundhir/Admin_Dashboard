f7f88d58bdb2830a15d31cae104b794910770e53
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-16 23:59:07 +0530

    navbar jsx file updated.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar w-full p-[20px] flex items-center">
       <div className="logo">
         <img src="logo.svg" alt="logo" />
-        <span>Admin</span>
+        <span>lamadmin</span>
       </div>
-      <div className="icons flex">
-        <img src="/serach.svg" alt="" className="icon" />
-        <img src="/app.svg" alt="" className="icon" />
-        <img src="/expand.svg" alt="" className="icon" />
+      <div className="icons flex items-center">
+        <img src="/search.svg" alt="search" className="icon" />
+        <img src="/app.svg" alt="app" className="icon" />
+        <img src="/expand.svg" alt="expand" className="icon" />
         <div className="notification">
-          <img src="/notification.svg" alt="" />
+          <img src="/notifications.svg" alt="notification"/>
           <span>1</span>
         </div>
-        <div className="user flex">
-          <img src="/profile.jpg" alt="profile" />
+        <div className="user flex items-center">
+          <img src="/profile.jpg" alt="profile" className='img w-[26px] h-[26px] rounded-full object-cover' />
           <span>Vyom</span>
         </div>
         <img src="/settings.svg" alt="" className="icon" />
