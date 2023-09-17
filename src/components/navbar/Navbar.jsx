a62db1ee21f4f8da40f4786a28b82d0890fa38a2
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-17 23:43:01 +0530

    Navbar jsx file updated.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,25 +2,35 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="navbar w-full p-[20px] flex items-center">
-      <div className="logo">
+    <div className="navbar w-full p-[20px] flex items-center justify-between ">
+
+      <div className="logo flex items-center font-[700]">
         <img src="logo.svg" alt="logo" />
         <span>lamadmin</span>
       </div>
-      <div className="icons flex items-center">
+
+      <div className="icons flex items-center gap-[20px]">
+
         <img src="/search.svg" alt="search" className="icon" />
+
         <img src="/app.svg" alt="app" className="icon" />
+
         <img src="/expand.svg" alt="expand" className="icon" />
-        <div className="notification">
+
+        <div className="notification relative">
           <img src="/notifications.svg" alt="notification"/>
-          <span>1</span>
+          <span className='flex items-center justify-center bg-[red] text-[white] w-[16px] h-[16px] rounded-full absolute top-[-10px] right-[-10px] text-[12px]'>1</span>
         </div>
-        <div className="user flex items-center">
+
+        <div className="user flex items-center gap-[10px]">
           <img src="/profile.jpg" alt="profile" className='img w-[26px] h-[26px] rounded-full object-cover' />
           <span>Vyom</span>
         </div>
-        <img src="/settings.svg" alt="" className="icon" />
+
+        <img src="/settings.svg" alt="settings" className="icon" />
+
       </div>
+
     </div>
   )
 }
