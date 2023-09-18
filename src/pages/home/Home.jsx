f9f8acd35bb25311e7a08464bbb24e9300536bc6
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-18 23:55:32 +0530

    Home jsx file updated.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home grid gap-[20px] grid-cols-4'>
+      <div className="box box1 p-[20px] rounded-[10px] border-[1px] border-[#384256]">Box1</div>
+      <div className="box box2 p-[20px] rounded-[10px] border-[1px] border-[#384256]">Box2</div>
+      <div className="box box3 p-[20px] rounded-[10px] border-[1px] border-[#384256]">Box3</div>
+      <div className="box box4 p-[20px] rounded-[10px] border-[1px] border-[#384256]">Box4</div>
+      <div className="box box5 p-[20px] rounded-[10px] border-[1px] border-[#384256]">Box5</div>
+      <div className="box box6 p-[20px] rounded-[10px] border-[1px] border-[#384256]">Box6</div>
+      <div className="box box7 p-[20px] rounded-[10px] border-[1px] border-[#384256]">Box7</div>
+      <div className="box box8 p-[20px] rounded-[10px] border-[1px] border-[#384256]">Box8</div>
+      <div className="box box9 p-[20px] rounded-[10px] border-[1px] border-[#384256]">Box9</div>
+    </div>
   )
 }
 
