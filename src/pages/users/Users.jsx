0cd08c6f6d3688e4005b48f912e886aff13714f9
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-23 23:46:56 +0530

    Users jsx file modified.

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import DataTable from '../../components/dataTable/DataTable'
 
 const Users = () => {
   return (
-    <div>Users</div>
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable/>
+    </div>
   )
 }
 
