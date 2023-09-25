5fbfb700d3201ef57cf2a17d6ce9a8f304417024
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-09-25 23:55:10 +0530

    Users jsx file modified.

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,5 +1,54 @@
 import React from 'react'
 import DataTable from '../../components/dataTable/DataTable'
+import { userRows } from '../../data';
+
+const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img className='w-[32px] h-[32px] rounded-full object-cover' src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 150,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 150,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
 
 const Users = () => {
   return (
@@ -8,7 +57,7 @@ const Users = () => {
         <h1>Users</h1>
         <button>Add New User</button>
       </div>
-      <DataTable/>
+      <DataTable columns={columns} rows={userRows}/>
     </div>
   )
 }
