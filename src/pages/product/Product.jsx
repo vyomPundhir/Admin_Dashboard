72cb74299e52d46f96a0b4444ee0df1e3fe10a64
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-08 22:59:23 +0530

    Product jsx added.

---
+++ src/pages/product/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Single from "../../components/single/Single"
+
+const Product = () => {
+
+  // Fetch data and send to Single component
+  return (
+    <div className="product" >
+      <Single />
+    </div>
+  )
+}
+
+export default Product
