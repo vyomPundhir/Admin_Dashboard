63ed17b28c1bbfc6bf5b9537b44fcda6ca4d4978
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-08 22:59:34 +0530

    User jsx added.

---
+++ src/pages/user/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Single from "../../components/single/Single"
+
+const User = () => {
+
+  // Fetch data and send to Single component
+
+  return (
+    <div className="user" >
+      <Single />
+    </div>
+  )
+}
+
+export default User
