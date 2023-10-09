ab01c65d291397d93da248e00509044421bb8f2b
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-09 23:56:10 +0530

    Product jsx file updated

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Single from "../../components/single/Single"
+import {singleProduct} from "../../data"
 
 const Product = () => {
 
   // Fetch data and send to Single component
   return (
     <div className="product" >
-      <Single />
+      <Single {...singleProduct}/>
     </div>
   )
 }
