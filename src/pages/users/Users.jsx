2ff5554248d3546477ddf321f541dcda112d5842
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-10 23:51:22 +0530

    users jsx file updated.

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import DataTable from '../../components/dataTable/DataTable'
 import { userRows } from '../../data';
 import Add from '../../components/add/Add';
+import { useQuery } from '@tanstack/react-query';
 
 const columns = [
   { field: "id", headerName: "ID", width: 90 },
@@ -55,6 +56,14 @@ const Users = () => {
 
   const [open, setOpen] = useState(false)
 
+  const { isLoading, data } = useQuery({
+    queryKey: ['allusers'],
+    queryFn: () =>
+      fetch("http://localhost:8800/api/users").then(
+        (res) => res.json(),
+      ),
+  })
+
   return (
     <div className="users">
       <div className="info flex items-center gap-[20px] mb-[20px]">
@@ -64,7 +73,10 @@ const Users = () => {
           Add New User
         </button>
       </div>
-      <DataTable slug="users" columns={columns} rows={userRows}/>
+      {isLoading ? "Loading..." : (
+        <DataTable slug="users" columns={columns} rows={data}/>
+      )}
+      {/* <DataTable slug="users" columns={columns} rows={userRows}/> */}
       {open && <Add slug="user" columns={columns} setOpen={setOpen}/>}
     </div>
   )
